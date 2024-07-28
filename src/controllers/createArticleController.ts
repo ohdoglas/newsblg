ba1b31e33009f8dde2077da0d53ab49ec976e6b3
Douglas Morais <dughlaisdd@gmail.com>
2024-07-28 10:24:19 -0300

    Config<Generate content(article) controller>

--- src/controllers/createArticleController.ts
+++ src/controllers/createArticleController.ts
@@ -1,13 +1,6 @@
 import { Request, Response } from "express";
 import generateContent from "../services/generateContent";
 
-
-export function newsBlgHome (req: Request, res: Response) {
-    return res.status(200).json({
-        mensagem: 'NewsBLG API running'
-    })
-}
-
 export async function createArticle (req: Request, res: Response) {
     const { topic } = req.body;
     try {
