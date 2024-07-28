1b99c9d254ccec21b57cc2ee384ef45d7c6d1343
Douglas Morais <dughlaisdd@gmail.com>
2024-07-28 10:17:07 -0300

    Feature<Generate content(article) controller implement>

--- src/controllers/controllers.ts
+++ src/controllers/controllers.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import generateContent from "../services/generateContent";
 
 
 export function newsBlgHome (req: Request, res: Response) {
@@ -6,3 +7,13 @@ export function newsBlgHome (req: Request, res: Response) {
         mensagem: 'NewsBLG API running'
     })
 }
+
+export async function createArticle (req: Request, res: Response) {
+    const { topic } = req.body;
+    try {
+        const content = await generateContent(topic);
+        res.status(200).json({ article: content});
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to generate article'});
+    }
+};
