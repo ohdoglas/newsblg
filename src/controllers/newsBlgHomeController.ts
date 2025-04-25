34c35fec1372f308a7c26a21bff32c20ffb80e47
ohdoglas <dughlaisdd@gmail.com>
2025-04-25 13:24:53 -0300

    change/update/fix: backend folders re-structured

--- src/controllers/newsBlgHomeController.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-
-export function newsBlgHome (req: Request, res: Response) {
-    return res.status(200).json({
-        mensagem: 'NewsBLG API running'
-    })
-}
-
